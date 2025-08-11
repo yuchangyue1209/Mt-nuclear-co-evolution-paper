c5c6b56b5cd8d44582b9841dd048804c7c9ee853
yuchangyue1209 <changyue.yu@wisc.edu>
2025-08-11 12:49:28 -0500

    add erc

--- Scripts/Untitled-4.sh
+++ Scripts/Untitled-4.sh
@@ -1,10 +1,11 @@
 # dn/ds calculation
 #Merge the files into one fasta
 cat /work/cyu/poolseq/PPalign_output/overlap.vcf/consensus/*.fasta > /work/cyu/poolseq/PPalign_output/overlap.vcf/consensus/all_mt_overlap.fasta
-
+/mnt/spareHD_2/AK_bam/consensus_individual_mt
 13. Realignment
 mafft --auto all_mt_overlap.fasta > aligned_mt_overlap.fasta
 muscle -in all_mt_overlap.fasta -out mc_aligned_mt_overlap.fasta
+
 
 14. IQtree 
 iqtree -s aligned_mt_overlap.fasta -m GTR+G -bb 1000 -alrt 1000 -nt AUTO
